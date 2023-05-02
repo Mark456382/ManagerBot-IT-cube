442b447563429f4647c4d492e6c109ac2bbf8c42
Mark3306 <94844045+Mark456382@users.noreply.github.com>
2023-05-02 18:50:28 +0200

    add database
    add database

--- buttons.py
+++ buttons.py
@@ -9,3 +9,21 @@ keyboard.add(*buts).add(but1).add(helps)
 keyboard_1 = ReplyKeyboardMarkup()
 buts_1 = ['Сотрудник (исполнитель)', "Менеджер (управляющий)"]
 keyboard_1.add(*buts_1)
+
+keyb = ReplyKeyboardMarkup()
+b1 = KeyboardButton('13-15')
+b2 = KeyboardButton('16-18')
+b3 = KeyboardButton('18+')
+keyb.add(b1).add(b2).add(b3)
+
+keyb2 = ReplyKeyboardMarkup()
+b4 = KeyboardButton('Суши')
+b5 = KeyboardButton('Пицца')
+b6 = KeyboardButton('Вок')
+keyb2.add(b4).add(b5).add(b6)
+
+keyb3 = ReplyKeyboardMarkup()
+b7 = KeyboardButton('Ужасы')
+b8 = KeyboardButton('Драмма')
+b9 = KeyboardButton('Комедия')
+keyb3.add(b7).add(b8).add(b9)
--- config.py
+++ config.py
@@ -1,1 +1,12 @@
 TOKEN = '5588087883:AAFTE4wauSpWfjp-XajBLpk9TVArA0Lwgfw'
+
+DATABASE = {
+    'drivername': 'postgres',
+    'host': 'localhost',
+    'port': '5432',
+    'username': 'postgres',
+    'password': '1234',
+    'database': 'testdb'
+}
+
+ENGINE = 'postgresql+psycopg2://postgres:1234@localhost/testdb'
